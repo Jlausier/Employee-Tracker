15b7c752269b359075a099d08dc8847a9eb3b9b4
Jacob <jakelausier7878@gmail.com>
2023-08-03 15:07:42 -0400

    added dotenv files with sequlize

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
-const mysql = require('mysql2');
-
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'rootroot',
-  database: 'employees'
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  }
+);
+
+
+
 
 module.exports = connection;
