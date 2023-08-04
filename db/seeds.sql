f9da054eeb9dcbb070cd1a36585ba500514fffd8
Jacob <jakelausier7878@gmail.com>
2023-08-04 12:14:32 -0400

    changed salary's, changed deparments

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
 INSERT INTO department(department_name)
-VALUES("Engineering"), ("Sales"), ("Finance"), ("Legal"), ("Marketing");
+VALUES("Engineering"), ("Web Development"), ("Customer Service"), ("Legal"), ("Marketing");
 
 INSERT INTO role(title, salary, department_id)
-VALUES("Engineer", 85000, 1), ("Senior Engineer", 125000, 1), ("CFO", 350000, 3), ("Chief Counsel", 300000, 4);
+VALUES("Engineer", 80000, 1), ("Senior Engineer", 127050, 1), ("CFO", 35120, 3), ("Junior Developer", 82000, 4);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ('Joe', 'Shmoe', 1, 2), ('tyrique', 'Smith', 1, null), ('Mallory', 'Manning', 1, 2), ('Slim', 'Jim', 2, 2), ('Adam', 'Legal', 4, null);
+VALUES ('Joe', 'Shmoe', 1, 2), ('tyrique', 'Smith', 1, null), ('Mallory', 'Manning', 1, 2), ('Slim', 'Jim', 2, 2), ('Adam', 'Web Development', 4, null);
