3aee71e3433adb6dfc4f9934e5e0e3acca2be345
Jacob <jakelausier7878@gmail.com>
2023-08-04 12:11:47 -0400

    changed names in db

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,4 +5,4 @@ INSERT INTO role(title, salary, department_id)
 VALUES("Engineer", 85000, 1), ("Senior Engineer", 125000, 1), ("CFO", 350000, 3), ("Chief Counsel", 300000, 4);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
-VALUES ('Johnnie', 'Random', 1, 2), ('James', 'Smith', 1, null), ('Ronnie', 'Manning', 1, 2), ('Jimmy', 'Jones', 2, 2), ('Larry', 'Legal', 4, null);
+VALUES ('Joe', 'Shmoe', 1, 2), ('tyrique', 'Smith', 1, null), ('Mallory', 'Manning', 1, 2), ('Slim', 'Jim', 2, 2), ('Adam', 'Legal', 4, null);
